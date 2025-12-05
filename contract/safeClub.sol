426210ff45046675c4705ee4fc1d02995abf0ab8
tebeerr <teberramzi@gmail.com>
2025-12-05 17:34:35 +0100

    feature is member function

--- contract/safeClub.sol
+++ contract/safeClub.sol
@@ -15,7 +15,7 @@ pragma solidity >=0.8.2 <0.9.0;
     uint256 public proposalCount ;
 
     address[] public members;                      
-
+    mapping (address => bool) public isMember;
 
     event MemberAdded(address member);
     event MemberRemoved(address member);
@@ -24,17 +24,25 @@ pragma solidity >=0.8.2 <0.9.0;
         owner = msg.sender;           
         members.push(owner);
         memberCount = 1;
+        isMember[owner] = true;
     }
 
     modifier _onlyOwner (){
-        require(msg.sender == owner , "only owner has the right to do this task");
+        require(msg.sender == owner , "only owner has the right to do this task !");
+        _;
+    }
+
+    modifier _onlyMember (){
+        require(isMember[msg.sender] , "only member has the right to this task !");
         _;
     }
 
 
     function addMember (address _member) public _onlyOwner{
         require(_member != address(0), "Invalid address");
-
+        require(!isMember[_member], "this Member already exists");
+        
+        isMember[_member] = true ;
         members.push(_member);
         memberCount++;
         
@@ -43,12 +51,20 @@ pragma solidity >=0.8.2 <0.9.0;
 
 
     function removeMember (address _member ) public _onlyOwner {
+        require(isMember[_member] , "Not a member");
         require(_member != owner, "Cannot remove owner");
 
+        isMember[_member] = false;
         memberCount--;
 
         emit MemberRemoved(_member);
     }
 
+    function getAllMembers() public view returns(address[] memory) {
+        return members;
+    }
+
+
+    
 
  }
